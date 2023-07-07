8c4e7f2fe4501766a82a2d86d2b9966fd7093b3c
Zach Trocinski <ztrocinski@outlook.com>
2023-07-07 13:11:21 -0500

    Fixed function results to not return string

--- .github/actions-pester/PolicyPesterTestHelper.psm1
+++ .github/actions-pester/PolicyPesterTestHelper.psm1
@@ -20,12 +20,15 @@ function Get-PolicyFiles
         [String]$PRBranch = "$($env:GITHUB_HEAD_REF)"
     )
 
+    $PolicyAndSetFiles = @()
+
     $PolicyFiles = @(git diff --diff-filter=$DiffFilter --name-only origin/main $PRBranch -- $PolicyDir)
     $PolicySetsFiles = @(git diff --diff-filter=$DiffFilter --name-only origin/main $PRBranch -- $PolicySetDir)
 
     $PolicyAndSetFiles = $PolicyFiles + $PolicySetsFiles
 
-    $PolicyAndSetFiles | ForEach-Object {
-        Write-Output $_
+    ForEach ($File in $PolicyAndSetFiles)
+    {
+        return $File
     }
 }
